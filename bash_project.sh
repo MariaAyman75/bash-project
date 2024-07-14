66e995c92a0405cdd495705de6970856e2b19195
Stephania-Ehab <stephania1102001@gmail.com>
2024-07-14 03:37:44 +0300

    function of create tables

--- bash_project.sh
+++ bash_project.sh
@@ -57,3 +57,32 @@ drop_database() {
     echo "Database does not exist!"
   fi
 }
+# Function to create a table
+create_table() {
+  echo "Enter table name: "
+  read tablename
+  if [[ ! "$tablename" =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
+    echo "Invalid table name! It must start with a letter or underscore and contain only alphanumeric characters and underscores."
+  elif [ -f "$tablename" ]; then
+    echo "Table already exists!"
+  else
+    echo "Enter number of columns: "
+    read colnum
+    columnsname=()
+    datatype=()
+    for (( i=1; i<=colnum; i++ ))
+    do
+      echo "Enter column $i name: "
+      read colname
+      echo "Enter column $i data type int or string: "
+      read coltype
+      columnsname+=("$colname")
+      datatype+=("$coltype")
+    done
+    IFS=':' 
+    echo "${columnsname[*]}" >> "$tablename"
+    echo "${datatype[*]}" >> "$tablename"
+   
+    echo "Table created!"
+  fi
+}
