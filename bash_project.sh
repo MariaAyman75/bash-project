57e7286fd843074aa2ba47a76e2b91f306960538
Stephania-Ehab <stephania1102001@gmail.com>
2024-07-14 04:03:01 +0300

    function of main menu of table

--- bash_project.sh
+++ bash_project.sh
@@ -354,3 +354,31 @@ update_table() {
     echo "Table does not exist!"
   fi
 }
+# Database menu
+database_menu() {
+  while true
+   do
+    echo "Database Menu:"
+    echo "1. Create Table"
+    echo "2. List Tables"
+    echo "3. Drop Table"
+    echo "4. Insert into Table"
+    echo "5. Select From Table"
+    echo "6. Delete From Table"
+    echo "7. Update Table"
+    echo "8. Exit"
+    echo "Choose an option: "
+    read option
+    case $option in
+      1) create_table ;;
+      2) list_tables ;;
+      3) drop_table ;;
+      4) insert_into_table ;;
+      5) select_from_table ;;
+      6) delete_from_table ;;
+      7) update_table ;;
+      8) cd ../ ; break ;;
+      *) echo "Invalid option!" ;;
+    esac
+  done
+}
